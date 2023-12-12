876160e2fa15b075bcbc86f347fe3eef630807c1
rismailov <rismailov@protonmail.com>
2023-12-13 00:19:04 +0400

    change input placeholder color

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -11,7 +11,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
             <input
                 type={type}
                 className={cn(
-                    'flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
+                    'flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-foreground/40 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
                     // custom
                     'text-foreground focus-visible:ring-2 focus-visible:ring-foreground dark:focus-visible:ring-indigo-300 font-paragraph',
                     className,
@@ -22,6 +22,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
         )
     },
 )
+
 Input.displayName = 'Input'
 
 export { Input }
